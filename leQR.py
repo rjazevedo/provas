7274a0105598c3ffe04483419feea1d6a6f68dbb
Rodolfo Azevedo <rodolfo@ic.unicamp.br>
2019-04-23 15:05:16 -0300

    criado leitor de QR para teste geral

--- leQR.py
+++ leQR.py
@@ -9,13 +9,7 @@ import sys
 
 if __name__ == '__main__':
     for arquivo in sys.argv[1:]:
-        print(arquivo)
         img = Image.open(arquivo)
-        w, h = img.size
-        img = img.crop((int(0.6 * w), 0, w, int(0.4* h)))
         qr = decode(img)
         if len(qr) == 1:
-            print(qr[0].data)
-        else:
-            img.save('crop-' + arquivo)
-
+            print(arquivo, qr[0].data)
