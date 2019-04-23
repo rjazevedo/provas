bc3d048527d0f2a9c7236c08634c952bdf5f5ef6
Rodolfo Azevedo <rodolfo@ic.unicamp.br>
2019-04-23 14:44:05 -0300

    atualizado corte para 40% da imagem em cada eixo

--- cortaQR.py
+++ cortaQR.py
@@ -4,12 +4,18 @@
 """ Corta o retângulo do canto superior direito da imagem (QR-Code?)"""
 
 from PIL import Image
+from pyzbar.pyzbar import decode
 import sys
 
 if __name__ == '__main__':
     for arquivo in sys.argv[1:]:
+        print(arquivo)
         img = Image.open(arquivo)
         w, h = img.size
         img = img.crop((int(0.6 * w), 0, w, int(0.4* h)))
+        qr = decode(img)
+        if len(qr) == 1:
+            print(qr[0].data)
+
         img.save('crop-' + arquivo)
 
