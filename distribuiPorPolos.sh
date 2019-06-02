bdcd64d88e9ce6ad146681fcccbad67604876ffc
Rodolfo Azevedo <rodolfo@ic.unicamp.br>
2019-06-02 13:20:45 -0300

    removendo mensagens de erro dos scripts de organização das pastas

--- distribuiPorPolos.sh
+++ distribuiPorPolos.sh
@@ -4,5 +4,5 @@ echo "Ignore todas as mensagens de erro a seguir..."
 
 for a in `seq -f "%04.0f" 0 336`
 do 
-  mv *-$a-* $a
+  mv *-$a-* $a 2> /dev/null
 done
--- organizaProvasPolos.sh
+++ organizaProvasPolos.sh
@@ -5,8 +5,8 @@ echo "Ignore todas as mensagens de erro a seguir..."
 for a in `seq -f "%04.0f" 0 336`
 do 
   mkdir -p $a
-  mv lista_presenca_${a}* $a
-  mv folha_resposta_${a}* $a
-  mv folha_ocorrencia_${a}* $a
-  rmdir $a
+  mv lista_presenca_${a}* $a 2> /dev/null
+  mv folha_resposta_${a}* $a 2> /dev/null
+  mv folha_ocorrencia_${a}* $a 2> /dev/null
+  rmdir $a 2> /dev/null
 done
