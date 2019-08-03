253b5de08c8f4304a6afd0e383c8b6a8a708d17d
Guilherme Albuquerque Pinto <guilherme.pinto@univesp.br>
2019-08-03 08:11:06 -0300

    fixing JSON update (sqlachemy json is immutable)

--- folhasSGA.py
+++ folhasSGA.py
@@ -3,20 +3,21 @@
 
 """Carrega o link de uma folha de resposta, de uma prova, de um aluno, no SGA"""
 
-import sqlalchemy
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime, Float, Boolean, JSON, Table, ForeignKey, or_
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import func
+from sqlalchemy.orm.attributes import flag_modified
 import os
 import sys
 import argparse
 import db
-import datetime
 
 args = None
 
 offer_types = { 'regular': 1, 'dp': 2 }
+TEST_PATH = '/var/data/nfs/provas/' # está no .ENV no SGA
+
+def erro( str ):
+    print( "Erro: " + str )
+    sys.exit(1)
 
 def carregaFolha( 
                   ac,  # activity_code
@@ -31,67 +32,93 @@ def carregaFolha(
     """Carrega o link de uma folha de resposta, de uma prova, de um aluno, no SGA"""
 
     ####################
-    # TO DO
-    # Logs
+    # Inicia Sessão 
     ####################
+    sess = db.Session()
+    sess.autoflush = True  # default
 
     # disciplina
-    activity = db.session.query(db.CurricularActivities) \
-                         .filter(db.CurricularActivities.code == ac) \
-                         .first()
+    activity = sess.query(db.CurricularActivities) \
+                .filter(db.CurricularActivities.code == ac) \
+                .first()
 
     # oferta
-    offer = db.session.query(db.ActivityOffers) \
-                      .filter(db.ActivityOffers.calendar_id == cid) \
-                      .filter(db.ActivityOffers.offer_type == offer_types[st]) \
-                      .first()
+    offer = sess.query(db.ActivityOffers) \
+             .filter(db.ActivityOffers.calendar_id == cid) \
+             .filter(db.ActivityOffers.offer_type == offer_types[st]) \
+             .first()
 
     # aluno
-    student = db.session.query(db.Students).filter(db.Students.academic_register == ar).first()
+    student = sess.query(db.Students).filter(db.Students.academic_register == ar).first()
 
-    if not activity: return
-    if not offer: return
-    if not student: return
+    if not activity: erro( "Missing CurricularActivities: %s" % (ac) )
+    if not offer: erro( "Missing ActivityOffers: %d, %d" % (cid, offer_types[st]) )
+    if not student: erro( "Missing Students: %d" %(ar) )
 
     # registro
-    record = db.session.query(db.ActivityRecords) \
-                       .filter(db.ActivityRecords.student_id == student.id) \
-                       .filter(db.ActivityRecords.curricular_activity_id == activity.id) \
-                       .filter(db.ActivityRecords.activity_offer_id == offer.id) \
-                       .first()
+    record = sess.query(db.ActivityRecords) \
+              .filter(db.ActivityRecords.student_id == student.id) \
+              .filter(db.ActivityRecords.curricular_activity_id == activity.id) \
+              .filter(db.ActivityRecords.activity_offer_id == offer.id) \
+              .first()
 
     # prova
-    test = db.session.query(db.ActivityTests) \
-                     .filter(db.ActivityTests.code == tc) \
-                     .filter(db.ActivityTests.curricular_activity_id == activity.id) \
-                     .first()
+    test = sess.query(db.ActivityTests) \
+            .filter(db.ActivityTests.code == tc) \
+            .filter(db.ActivityTests.curricular_activity_id == activity.id) \
+            .first()
 
-    if not record: return
-    if not test: return
+    if not record: erro( "Missing ActivityRecord: %d, %d, %d" % (student.id, activity.id, offer.id) )
+    if not test: erro( "Missing ActivityTests: %s, %d" % (tc, activity.id) )
 
     # submissão (cria uma caso não exista)
-    submission = db.session.query(db.ActivityRecordSubmissions) \
-                           .filter(db.ActivityRecordSubmissions.activity_record_id == record.id) \
-                           .filter(db.ActivityRecordSubmissions.submission_type == st) \
-                           .first()
+    submission = sess.query(db.ActivityRecordSubmissions) \
+                  .filter(db.ActivityRecordSubmissions.activity_record_id == record.id) \
+                  .filter(db.ActivityRecordSubmissions.submission_type == st) \
+                  .first()
     if not submission:
         submission = db.ActivityRecordSubmissions(
                                                     activity_record_id = record.id,
                                                     submission_type = st,
-                                                    activity_test_id = test.id
+                                                    activity_test_id = test.id,
+                                                    created_at = func.now(),
+                                                    updated_at = func.now()
                                                  )
-        db.add(submission)
+        sess.add(submission)
     else:
         submission.activity_test_id = test.id
 
-    db.flush()
+    # anexo (cria um caso não exista)
+    attach = sess.query(db.Attachments) \
+                       .filter(db.Attachments.attach_reference_id == submission.id) \
+                       .filter(db.Attachments.attach_reference_type == 'ActivityRecordSubmission') \
+                       .filter(db.Attachments.attach_type == 'response_sheets') \
+                       .first()
 
-    # folha
+    if not attach:
+        attach = db.Attachments(
+                                  attach_reference_id = submission.id,
+                                  attach_reference_type = 'ActivityRecordSubmission',
+                                  attach_type = 'response_sheets',
+                                  created_at = func.now(),
+                                  updated_at = func.now()
+                               )
+        sess.add(attach)
 
-    # TO DO
+    if not attach.sheets_data: attach.sheets_data = []
 
+    has_sheet_n = False
+    for e in attach.sheets_data:
+        if e['number'] == n:
+            has_sheet_n = True
 
+    # carrega folha caso não exista
+    if not has_sheet_n:
+        attach.sheets_data.append( {'number': n, 'path': TEST_PATH + ls} )
+        
+    flag_modified(attach, "sheets_data")  # Sqlalchemy JSON é imutável por default
 
+    sess.commit()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Carrega o link de uma folha de resposta, de uma prova, de um aluno, no SGA')
@@ -129,17 +156,17 @@ if __name__ == '__main__':
     activity_code = 'AAG002'
     test_code = 'P001'
     academic_register = '1600311'
-    number = 1
-    link_sheet = '/home/provas/dados/SGA/provas/0001/20190425-0001-AAG002-P001-1600311-01.png'
+    number = 6
+    link_sheet = 'SGA/provas/0001/20190425-0001-AAG002-P001-1600311-06.png'
 
     carregaFolha( 
-                  activity_code,     # Cód. da disciplina
-                  test_code,         # Cód. da prova
-                  academic_register, # RA do aluno
-                  number,            # Número da folha
-                  link_sheet,        # Link do arquivo da folha de resposta
-                  submission_type,   ## Tipo da submissão
-                  calendar_id        ## ID do calendário
+                  activity_code,     # str, Cód. da disciplina
+                  test_code,         # str, Cód. da prova
+                  academic_register, # str, RA do aluno
+                  number,            # int, Número da folha
+                  link_sheet,        # str, Link do arquivo da folha de resposta
+                  submission_type,   ### str, Tipo da submissão
+                  calendar_id        ### int, ID do calendário
                 )
 
     ####################
