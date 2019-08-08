93a79655f1921226afe4e7309d42d79bd7b580a8
Rodolfo Azevedo <rodolfo@ic.unicamp.br>
2019-08-08 16:43:42 -0300

    incluido a opção de forcar a inclusão de folhas avulsas nas provas quando a prova inteira estiver em branco

--- incluiFolhasFaltantes.py
+++ incluiFolhasFaltantes.py
@@ -8,16 +8,18 @@ import csv
 import sys
 from utils import BuscaArquivos, LinhaProva, Arquivos2Dict
 import shutil
+import functools
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Inclui folhas em branco nas provas incompletas dos polos, removendo as pendências.')
+    parser = argparse.ArgumentParser(description='Inclui folhas em branco nas provas incompletas dos polos, removendo as pendências. É preciso que alguma folha da prova esteja presente ou que a opção -f seja usada')
     parser.add_argument('-e', '--entrada', type=str, required=True, help='Arquivo de entrada com descrição de todas as provas (.csv)')
     parser.add_argument('-n', '--numeroPolo', type=str, required=True, help='Numero do polo a preencher as folhas ausentes (4 dígitos)')
     parser.add_argument('-p', '--provas', type=str, required=True, help='Pasta com as provas')
     parser.add_argument('-d', '--disciplina', type=str, required=False, help='Processa somente a disciplina especificada')
     parser.add_argument('-a', '--aluno', type=str, required=False, help='Processa somente o aluno especificado')
     parser.add_argument('-t', '--teste', action='store_true', required=False, help='Testa a execução (não realiza as cópias nada)')
+    parser.add_argument('-f', '--forcar', action='store_true', required=False, help='Força a criação das folhas quando não há nenhuma folha de uma prova')
     parser.add_argument('-v', '--verbose', action='store_true', required=False, help='Indica as folhas que foram incluidas')
 
     folhaBranca = os.path.join(os.path.dirname(sys.argv[0]), 'folha-nao-digitalizada.png')
@@ -39,8 +41,13 @@ if __name__ == '__main__':
         if prova.polo == args.numeroPolo:
             if args.disciplina is None or prova.disciplina == args.disciplina:
                 if args.aluno is None or prova.ra == args.aluno:
-                    for folha in prova.idPaginas():
-                        if not folha in entrada:
+                    paginas = prova.idPaginas()
+                    presentes = [f in entrada for f in paginas]
+                    temFolha = functools.reduce(lambda a, b: a or b, presentes)
+                    if not temFolha and not args.forcar:
+                        continue
+                    for (folha, presente) in zip(paginas, presentes):
+                        if not presente:
                             if args.teste:
                                 print(folha)
                             else:
