30938b8d0df393e39a7d9ae161dc99cb4c0bf186
Daniel Consiglieri <daniel.consiglieri@DESKTOP-MME4UJ5.univesp.local>
2019-08-12 11:56:06 -0200

    adicionando primeiro script auxiliar para teste

---
+++ scripts-auxiliares/dashboard-2BIM2019.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd ~/tmp
+mkdir d2BIM2019
+echo "Inicio da geração do DashBoard - 2 Bimestre 2019"
+~/src/dashboard.py -e ~/dados/SGA/provas_2_bimestre -p ~/dados/gdrive_rclone/2_bimestre/origem/csv/dashboard-2BIM-2019.csv -s ~/tmp/d2BIM2019 -a ~/dados/SGA/provas_2_bimestre/ausentes.csv
+cd ~/tmp/d2BIM2019
+cp -f *.html ~/dados/SGA/dashboard/2bim
+cd ~/tmp
+rm -rf d2BIM2019
