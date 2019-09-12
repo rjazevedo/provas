3543859f06584b9e288dfd64a834067363d0e477
Daniel Consiglieri <daniel.consiglieri@DESKTOP-MME4UJ5.univesp.local>
2019-09-12 10:12:53 -0200

    script bug fix

--- scripts-auxiliares/segundaEtapaManual.sh
+++ scripts-auxiliares/segundaEtapaManual.sh
@@ -17,10 +17,15 @@ PATH_PROVAS_1DP="/home/provas/dados/SGA/2019dp1/provas"
 PATH_PROVAS_EXAME_1BIM="/home/provas/dados/SGA/2019e1/provas"
 PATH_PROVAS_EXAME_2BIM=""
 PATH_LIMBO="/home/provas/dados/tmp/Limbo"
+
 #Opcoes de comando
 ACTION="cp -n"
 FORCED_ACTION="cp -f"
 #Fim das opcoes de comando
+
+mkdir Movidos_1BIM
+mkdir Movidos_2BIM 
+mkdir Movidos_1DP 
 
 echo "acessando a pasta de Saida..."
 cd Saida/
@@ -46,7 +51,7 @@ do
 	-o -iname '20190426*png' \
 	-o -iname '20190427*png' \
 	-o -iname '20190429*png' \
-	-o -iname '20190502*png' \) -exec ${ACTION} '{}' ${PATH_PROVAS_1BIM}/$a  \; -exec echo "Copiando " '{}' "para ${PATH_PROVAS_1BIM}/$a" \; -exec mkdir ../Movidos_1BIM \; -exec mv '{}' ../Movidos_1BIM \;
+	-o -iname '20190502*png' \) -exec ${ACTION} '{}' ${PATH_PROVAS_1BIM}/$a  \; -exec echo "Copiando " '{}' "para ${PATH_PROVAS_1BIM}/$a" \; -exec mv '{}' ../Movidos_1BIM \;
 	#Provas Regulares Segundo Bimestre
 	find ./$a/ -type f  \( -iname '20190626*png' \
 	-o -iname '20190627*png' \
@@ -57,13 +62,13 @@ do
 	-o -iname '20190705*png' \
 	-o -iname '20190706*png' \
 	-o -iname '20190711*png' \
-	-o -iname '20190712*png' \) -exec ${ACTION} '{}' ${PATH_PROVAS_2BIM}/$a  \; -exec echo "Copiando " '{}' "para ${PATH_PROVAS_2BIM}/$a" \; -exec mkdir ../Movidos_2BIM \; -exec mv '{}' ../Movidos_2BIM \;
+	-o -iname '20190712*png' \) -exec ${ACTION} '{}' ${PATH_PROVAS_2BIM}/$a  \; -exec echo "Copiando " '{}' "para ${PATH_PROVAS_2BIM}/$a" \; -exec mv '{}' ../Movidos_2BIM \;
 	#Prova 1DP
 	find ./$a/ -type f  \( -iname '20190610*png' \
 	-o -iname '20190611*png' \
 	-o -iname '20190612*png' \
 	-o -iname '20190617*png' \
-	-o -iname '20190619*png' \) -exec ${ACTION} '{}' ${PATH_PROVAS_1DP}/$a  \; -exec echo "Copiando " '{}' "para ${PATH_PROVAS_1DP}/$a" \; -exec mkdir ../Movidos_1DP \; -exec mv '{}' ../Movidos_1DP \;
+	-o -iname '20190619*png' \) -exec ${ACTION} '{}' ${PATH_PROVAS_1DP}/$a  \; -exec echo "Copiando " '{}' "para ${PATH_PROVAS_1DP}/$a" \; -exec mv '{}' ../Movidos_1DP \;
 	#Arquivos Limbo, sao os que nenhum padrao foi encontrado
 	find ./$a/ -type f -iname '*png' -exec ${ACTION} '{}' ${PATH_LIMBO}/$a  \; -exec echo "Copiando " '{}' "para ${PATH_LIMBO}/$a" \;	
 done
