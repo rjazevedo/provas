0d5e10d6531b5a5b66dd9e0d7a016900eda352d7
Daniel Consiglieri <daniel.consiglieri@DESKTOP-MME4UJ5.univesp.local>
2019-09-20 17:40:40 -0200

    Melhorias no fileListener e criacao de script auxiliar

--- listener/fileListenerAndProcessorParam.sh
+++ listener/fileListenerAndProcessorParam.sh
@@ -51,7 +51,11 @@ copy_file_and_execute_decodeqr()
   ##################
   # script do Daniel (adaptado por Guilherme, 6/8/2019)
   echo "Copiando para pasta final"
-  cp -n ${HOME}/${SAVEDIR}/${polo}/*  ${HOME}/${PATH_PROVAS}/${polo}
+  #--codigo antigo: cp -n ${HOME}/${SAVEDIR}/${polo}/*  ${HOME}/${PATH_PROVAS}/${polo}
+  #Nova rotina que coloca os arquivos no local correto
+  
+  ${HOME}/src/listener/move_arquivos.sh "${HOME}/${SAVEDIR}/${polo}" ${polo}
+  #
   ##################
 }
 
