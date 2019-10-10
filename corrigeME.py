3421cc7113afa909de1c01965c9efa2400efdcf5
Rodolfo Azevedo <rodolfo@ic.unicamp.br>
2019-10-10 17:37:09 -0300

    ajustando corretor de gabarito para suportar múltiplos formatos de variação de prova

--- corrigeME.py
+++ corrigeME.py
@@ -131,6 +131,7 @@ if __name__ == '__main__':
     gabaritos = {}
     for l in gabarito:
         g = Gabarito(l)
+        print('*', g.Nome())
         gabaritos[g.Nome()] = g
         
     # Lê as variações dos gabaritos. As variações tem o código do polo junto com a prova
