6d5ed5f01ddceca40366a24104571a301e9383b7
Rodolfo Azevedo <rodolfo@ic.unicamp.br>
2019-10-18 22:12:43 -0300

    atualizando corretor

--- sgaCorrigeFolhaInvalida.py
+++ sgaCorrigeFolhaInvalida.py
@@ -22,7 +22,9 @@ for att in db.session.query(db.Attachments).all():
             if pageInfo['path'].startswith('/var/data/nfs/provas//home/provas/dados/SGA/2019b3/provas/'): 
                 arquivo = pageInfo['path'].replace('/home/provas/dados/', '')
                 print(pageInfo['path'], '--->', arquivo)
-                
+                pageInfo['path'] = arquivo
+                modificado = True
+            
         if modificado:
             flag_modified(att, "sheets_data")
             db.session.commit()
