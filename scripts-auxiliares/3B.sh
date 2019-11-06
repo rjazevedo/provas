fde280a6cbe7b709fa066ed92a94b9f8e4bbc281
Daniel Consiglieri <daniel.consiglieri@DESKTOP-MME4UJ5.univesp.local>
2019-11-06 17:49:34 -0200

    Melhorias de desempenho

--- scripts-auxiliares/3B.sh
+++ scripts-auxiliares/3B.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd ~/src/scripts-auxiliares
-./3Bfipa.sh config/2019b3-conf.sh
+#./3Bfipa.sh config/2019b3-conf.sh -- Desnecessário, pois populaDB emabaralhado não libera folhas parciais diferente da versão regular
 ./3Bpcbpa.sh config/2019b3-conf.sh
 ./3Bpa.sh config/2019b3-conf.sh
