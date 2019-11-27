91103e5cedb1584f72d2805b6ab8fc76429256d0
Daniel Consiglieri <daniel.consiglieri@DESKTOP-MME4UJ5.univesp.local>
2019-11-27 18:14:39 -0200

    Nova funcionalidade de reversao de status aplicada a todos os periodos em move_arquivos.sh"

--- listener/move_arquivos.sh
+++ listener/move_arquivos.sh
@@ -18,7 +18,6 @@ DP1Semestre2019 - 20190623
 
 DATAS_CORINGAS
 
-
 PATH_PROVAS_1BIM="/home/provas/dados/SGA/provas"
 PATH_PROVAS_2BIM="/home/provas/dados/SGA/2019b2/provas"
 PATH_PROVAS_3BIM="/home/provas/dados/SGA/2019b3/provas"
@@ -37,7 +36,6 @@ MOVE_ACTION="mv"
 VERBOSE_MODE=""
 
 
-
 #Deve ser passado por parametro o arquivo de configuracao
 if [ "$#" -lt 2 ]; then
 	echo ${0} " Necessita de dois ou três parametros para funcionar"
@@ -49,7 +47,6 @@ else
 	if [[ "$3" == "-m" ]]; then
 		ACTION=${MOVE_ACTION}
 	fi
-	#echo "Opcao parametro: "${3}	
 #Inicio das rotinas
 	for lista in $( find ${PATH_ORIGEM} -iname '*png' );
 	do
@@ -60,9 +57,8 @@ else
 				${ACTION} "${lista}" ${PATH_LIMBO}
 				;;
 			*20190417*|*20190418*|*20190422*|*20190423*|*20190424*|*20190425*|*20190426*|*20190427*|*20190429*|*20190502*|*20190509*|*20190512*)
-	
 
-					echo "Detectei prova do 1o bimestre 2019 - ${ACTION} ${lista} ${PATH_PROVAS_1BIM}/${2}"
+				echo "Detectei prova do 1o bimestre 2019 - ${ACTION} ${lista} ${PATH_PROVAS_1BIM}/${2}"
 				
 				${ACTION} "${lista}" ${PATH_PROVAS_1BIM}/${2}
 				
@@ -72,13 +68,16 @@ else
 					temp="${temp%.*}"
 					rm ${PATH_PROVAS_1BIM}/${2}/result/${temp}.txt 2> /dev/null
 					rm ${PATH_PROVAS_1BIM}/${2}/${temp}.csv 2> /dev/null
+					#reverte o status de ausente e de ilegível
+					echo -e "${temp:0:8},${temp:9:4},${temp:14:6},${temp:21:4},${temp:26:7}" > ${WORKING_FOLDER}/reverte_status.csv
+					~/src/sgaPresentes.py -a ${WORKING_FOLDER}/reverte_status.csv -c 37 -e
+					rm ${WORKING_FOLDER}/reverte_status.csv
 				fi
 				
 				;;
 			*20190626*|*20190627*|*20190701*|*20190702*|*20190703*|*20190704*|*20190705*|*20190706*|*20190711*|*20190712*|*20190714*)
-			#data coringa
 
-					echo "Detectei prova do 2o bimestre 2019 - ${ACTION} ${lista} ${PATH_PROVAS_2BIM}/${2}"
+				echo "Detectei prova do 2o bimestre 2019 - ${ACTION} ${lista} ${PATH_PROVAS_2BIM}/${2}"
 
 				${ACTION} "${lista}" ${PATH_PROVAS_2BIM}/${2}
 				
@@ -88,12 +87,16 @@ else
 					temp="${temp%.*}"					
 					rm ${PATH_PROVAS_2BIM}/${2}/result/${temp}.txt 2> /dev/null
 					rm ${PATH_PROVAS_2BIM}/${2}/${temp}.csv 2> /dev/null
+					#reverte o status de ausente e de ilegível
+					echo -e "${temp:0:8},${temp:9:4},${temp:14:6},${temp:21:4},${temp:26:7}" > ${WORKING_FOLDER}/reverte_status.csv
+					~/src/sgaPresentes.py -a ${WORKING_FOLDER}/reverte_status.csv -c 38 -e
+					rm ${WORKING_FOLDER}/reverte_status.csv
 				fi
 				
 				;;
 			*20190610*|*20190611*|*20190612*|*20190617*|*20190619*|*20190623*)
 
-					echo "Detectei prova do DP 1 Semestre 2019 - ${ACTION} ${lista} ${PATH_PROVAS_1DP}/${2}"
+				echo "Detectei prova do DP 1 Semestre 2019 - ${ACTION} ${lista} ${PATH_PROVAS_1DP}/${2}"
 
 				${ACTION} "${lista}" ${PATH_PROVAS_1DP}/${2}
 				
@@ -103,12 +106,16 @@ else
 					temp="${temp%.*}"					
 					rm ${PATH_PROVAS_1DP}/${2}/result/${temp}.txt 2> /dev/null
 					rm ${PATH_PROVAS_1DP}/${2}/${temp}.csv 2> /dev/null
+					#reverte o status de ausente e de ilegível
+					echo -e "${temp:0:8},${temp:9:4},${temp:14:6},${temp:21:4},${temp:26:7}" > ${WORKING_FOLDER}/reverte_status.csv
+					~/src/sgaPresentes.py -a ${WORKING_FOLDER}/reverte_status.csv -c 39 -t dp -e
+					rm ${WORKING_FOLDER}/reverte_status.csv
 				fi	
 				
 				;;
 			*20190819*|*20190820*|*20190821*|*20190822*|*20190823*|*20190829*|*20190901*)
 
-					echo "Detectei prova Exame 1o bimestre 2019 - ${ACTION} ${lista} ${PATH_PROVAS_EXAME_1BIM}/${2}"
+				echo "Detectei prova Exame 1o bimestre 2019 - ${ACTION} ${lista} ${PATH_PROVAS_EXAME_1BIM}/${2}"
 
 				${ACTION} "${lista}" ${PATH_PROVAS_EXAME_1BIM}/${2}
 				
@@ -118,12 +125,16 @@ else
 					temp="${temp%.*}"					
 					rm ${PATH_PROVAS_EXAME_1BIM}/${2}/result/${temp}.txt 2> /dev/null
 					rm ${PATH_PROVAS_EXAME_1BIM}/${2}/${temp}.csv 2> /dev/null
+					#reverte o status de ausente e de ilegível
+					echo -e "${temp:0:8},${temp:9:4},${temp:14:6},${temp:21:4},${temp:26:7}" > ${WORKING_FOLDER}/reverte_status.csv
+					~/src/sgaPresentes.py -a ${WORKING_FOLDER}/reverte_status.csv -c 37 -t exam -e
+					rm ${WORKING_FOLDER}/reverte_status.csv
 				fi		
 				
 				;;
 			*20190909*|*20190910*|*20190911*|*20190912*|*20190913*|*20190916*|*20190920*|*20190922*)
 
-					echo "Detectei prova Exame 2o bimestre 2019 - ${ACTION} ${lista} ${PATH_PROVAS_EXAME_2BIM}/${2}"
+				echo "Detectei prova Exame 2o bimestre 2019 - ${ACTION} ${lista} ${PATH_PROVAS_EXAME_2BIM}/${2}"
 
 				${ACTION} "${lista}" ${PATH_PROVAS_EXAME_2BIM}/${2}
 				
@@ -133,12 +144,16 @@ else
 					temp="${temp%.*}"					
 					rm ${PATH_PROVAS_EXAME_2BIM}/${2}/result/${temp}.txt 2> /dev/null
 					rm ${PATH_PROVAS_EXAME_2BIM}/${2}/${temp}.csv 2> /dev/null
+					#reverte o status de ausente e de ilegível
+					echo -e "${temp:0:8},${temp:9:4},${temp:14:6},${temp:21:4},${temp:26:7}" > ${WORKING_FOLDER}/reverte_status.csv
+					~/src/sgaPresentes.py -a ${WORKING_FOLDER}/reverte_status.csv -c 38 -t exam -e
+					rm ${WORKING_FOLDER}/reverte_status.csv					
 				fi				
 				
 				;;
 			*20191002*|*20191003*|*20191007*|*20191008*|*20191009*|*20191010*|*20191011*|*20191014*|*20191016*|*20191017*|*20191018*|*20191021*|*20191022*|*20191024*)
 
-					echo "Detectei prova do 3o bimestre 2019 - ${ACTION} ${lista} ${PATH_PROVAS_3BIM}/${2}"
+				echo "Detectei prova do 3o bimestre 2019 - ${ACTION} ${lista} ${PATH_PROVAS_3BIM}/${2}"
 
 				${ACTION} "${lista}" ${PATH_PROVAS_3BIM}/${2}
 				
@@ -151,6 +166,7 @@ else
 					#reverte o status de ausente e de ilegível
 					echo -e "${temp:0:8},${temp:9:4},${temp:14:6},${temp:21:4}-${temp:9:4},${temp:26:7}" > ${WORKING_FOLDER}/reverte_status.csv
 					#como regular e DP estão misturadas é necessário chutar qual é o registro correto
+					echo "Atencao! Como os registros de DP e regular do 3BIM2019 estao misturados eh normal que um dos procedimentos falhe e o outro funcione"
 					~/src/sgaPresentes.py -a ${WORKING_FOLDER}/reverte_status.csv -c 44 -e
 					~/src/sgaPresentes.py -a ${WORKING_FOLDER}/reverte_status.csv -c 46 -t dp -e
 					rm ${WORKING_FOLDER}/reverte_status.csv
