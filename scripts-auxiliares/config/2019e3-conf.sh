94604c1adc56a8b61ca3ba9afbd01827fe7fd68d
Daniel Consiglieri <daniel_consiglieri@yahoo.com.br>
2020-03-04 16:21:53 -0200

    Atualizacao calendario consolidado

--- scripts-auxiliares/config/2019e3-conf.sh
+++ scripts-auxiliares/config/2019e3-conf.sh
@@ -7,7 +7,7 @@ BACKUP_CSV="/home/provas/dados/backup/$PERIODO"
 #Isso é invariavel, é uma base de busca de string
 BASE_STRING="Dashboard Provas UNIVESP"
 #Caso o bimestre tenha sido consolidado preencher "sim", caso contrario deixar em branco
-BIMESTRE_CONSOLIDADO=""
+BIMESTRE_CONSOLIDADO="sim"
 #Calendario SGA
 CALENDARIO="44"
 CALENDARIO_DP="46"
